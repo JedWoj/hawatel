cdaf0de5b4e23b6f6c70fa98ffc6926806d68773
JedWoj <jedwojc@gmail.com>
2022-10-07 23:01:09 +0200

    trycatch added to user page

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -3,23 +3,29 @@ import { UsersType } from "../../types/UserType";
 import UserList from '../../components/User/UsersList';
 
 interface UserPageType {
-    users: UsersType;
+    users?: UsersType;
+    error?: string;
 }
 
-const UserPage = ({users}: UserPageType) => {
-    return(
-        <UserList users={users.data} />
-    )
+const UserPage = ({users, error}: UserPageType) => {
+    return users ? <UserList users={users.data} /> : <div className="text-2xl sm:text-4xl md:text-6xl text-violet-700 text-center fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">{error}</div>;
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-    const response = await fetch('https://gorest.co.in/public/v1/users');
-    const users = await response.json();
-    
-    return {
-      props: { users },
-      revalidate: 120
+    try {
+        const response = await fetch('https://gorest.co.in/public/v1/users');
+        if(!response.ok) throw new Error();
+        const users = await response.json();
+        return {
+          props: { users },
+          revalidate: 120
+        }
+    } catch (error) {
+        return {
+            props: { error: 'Something went wrong!' }
+        }
     }
+    
 }
 
 export default UserPage;
